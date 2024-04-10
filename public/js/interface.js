d1863f48e00836bc782c846630d0ac459f1cca51
Zerithe <jonathandevera08@gmail.com>
2024-04-10 16:05:10 +0800

    Update interface.js

--- public/js/interface.js
+++ public/js/interface.js
@@ -357,7 +357,7 @@ importCsvVisMinBtn.addEventListener('click', async () => {
 
 const syncDataCentral = document.querySelector('#sync-data-central');
 syncDataCentral.addEventListener('click', () => {
-    const response = fetch('/syncdatacentral');
+    const response = fetch('/synccentral');
 });
 
 const syncDataLuzonVisMin = document.querySelector('#sync-data-luzon-vismin');
